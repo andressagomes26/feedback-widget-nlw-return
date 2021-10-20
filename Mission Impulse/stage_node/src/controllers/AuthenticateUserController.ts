1e359d07624d013991b9d348fcd167ee1b75a067
Andressa Gomes <andressa_gomes.12@hotmail.com>
2021-10-20 19:22:14 -0300

    Continuando stage 1

---
+++ Mission Impulse/stage_node/src/controllers/AuthenticateUserController.ts
@@ -0,0 +1,9 @@
+import { Request, Response} from "express";
+
+class AuthenticateUserService {
+    async handle(request: Request, response: Response){
+        const service = new AuthenticateUserService;
+    }
+}
+
+export { AuthenticateUserService }
---
+++ Mission Impulse/stage_node/src/services/AuthenticateUserService.ts
@@ -0,0 +1,9 @@
+import { Request, Response} from "express";
+
+class AuthenticateUserService {
+    async execute(code: string, response: Response){
+        
+    }
+}
+
+export { AuthenticateUserService }
