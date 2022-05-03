857d4451d8fc430ee66e4d7d35f24ffefd7a8500
Andressa Gomes Moreira <andressa_gomes.12@hotmail.com>
2022-05-02 21:29:43 -0300

    Aprendendo sobre o conceito de estado

--- return/aulas/web/src/App.tsx
+++ return/aulas/web/src/App.tsx
@@ -1,19 +1,7 @@
-interface ButtonProps {
-	text?: string;
-}
+import { Widget } from "./components/Widget";
 
-function Button(props: ButtonProps){
-	return <button className="bg-violet-500 p-2 h-10 rounded text-violet-100 hover:bg-violet-700 transition-colors">{props.text ?? 'default'}</button>
-}
-
-function App() {
-	return(
-		<div className="flex gap-2">
-			<Button text='Apagar' />
-			<Button text='Enviar' />
-			<Button />
-		</div>
-	)
+export function App() {
+	return <Widget />
 }
 
 export default App
