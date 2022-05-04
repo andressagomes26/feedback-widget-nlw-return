93051902bad73b56709b021d26a91798530b8e5e
Andressa Gomes Moreira <andressa_gomes.12@hotmail.com>
2022-05-04 09:53:27 -0300

    Organizando os formulários de acordo com o tipo de feedback

---
+++ return/aulas/web/src/components/WidgetForm/Steps/FeedbackContentStep.tsx
@@ -0,0 +1,34 @@
+import { ArrowLeft } from "phosphor-react";
+import { FeedBackType, feedbackTypes } from "..";
+import { CloseButton } from "../../CloseButton";
+
+interface FeedbackContentStepProps {
+    feedbackType: FeedBackType;
+    // Recebe a propriedade feedbackType que é do tipo FeedBackType ("BUG" | "IDEA" | "OTHER")
+}
+
+export function FeedbackContentStep({feedbackType}: FeedbackContentStepProps){
+    const feedbackTypeInfo = feedbackTypes[feedbackType];
+    // feedbackTypes['BUG'] = todo o valor dessa chave
+
+    return (
+        <>
+            <header>
+                <button type="button" className="top-5 left-5 absolute text-zinc-400 hover:text-zinc-100">
+                    <ArrowLeft weight="bold" className="w-4 h-4"/>
+                </button>
+
+                <span className="text-xl leading-6 flex items-center gap-2">
+                    <img src={feedbackTypeInfo.image.source} alt={feedbackTypeInfo.image.alt} className="w-6 h-6" />
+                    {feedbackTypeInfo.title}
+                </span>
+
+                <CloseButton />
+            </header>
+
+            <div className="flex py-8 gap-2 w-full">
+                
+            </div>
+        </>
+    )
+}
