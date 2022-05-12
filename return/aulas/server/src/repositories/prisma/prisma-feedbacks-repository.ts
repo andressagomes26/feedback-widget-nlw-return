6e55919eb0480f143f916c1788dd3216eb6e03f8
Andressa Gomes Moreira <andressa_gomes.12@hotmail.com>
2022-05-12 08:39:15 -0300

    Fazendo deploy da aplicação

--- return/aulas/server/src/repositories/prisma/prisma-feedbacks-repository.ts
+++ return/aulas/server/src/repositories/prisma/prisma-feedbacks-repository.ts
@@ -2,7 +2,7 @@ import { prisma } from "../../prisma";
 import { FeedbackCreateData, FeedbacksRepository } from "../feedbacks-repository";
 
 export class PrismaFeedbacksRepository implements FeedbacksRepository {
-    async create({type, comment, screenshot}: FeedbackCreateData){
+    async create({ comment, type, screenshot }: FeedbackCreateData) {
         await prisma.feedback.create({
             data: {
                 type,
